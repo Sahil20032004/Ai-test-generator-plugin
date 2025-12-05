ebae3b0c1cfbb290ebbf96e3d6d54d9b0442b2d0
GitHub Action <action@github.com>
2025-12-05 10:55:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/sahil20032004/aitestgeneratorplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/sahil20032004/aitestgeneratorplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.sahil20032004.aitestgeneratorplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
